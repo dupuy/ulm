a230131a889395ad3b05ffdbdad3b2a41417ea40
Alexander Dupuy <alex.dupuy@mac.com>
2013-10-14 18:25:47 +0200

    Django 1.3 compatibility

--- ulm/urls.py
+++ ulm/urls.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+try:
+    from django.conf.urls import patterns, include, url
+except (ImportError):                   # Django 1.3 compatibility
+    from django.conf.urls.defaults import patterns, include, url
+        
 from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:

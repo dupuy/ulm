3ee49e532c99f8b695c0711e87e794d1dac77d09
Alexander Dupuy <alex.dupuy@mac.com>
2013-10-14 04:31:47 +0200

    Fixes to documentation generation
    Enable autodocs
    Ignore generated docs for git
    Also add _static/.gitignore so git will create the _static directory

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-#  documentation build configuration file, created by
-# sphinx-quickstart on Sun Feb 17 11:46:20 2013.
+# documentation build configuration file, created by sphinx-quickstart
 #
 # This file is execfile()d with the current directory set to its containing dir.
 #
@@ -16,7 +15,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -25,7 +24,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = [ 'sphinx.ext.autodoc' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,8 +39,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u''
-copyright = u'2013, ChangeMyName'
+project = u'ulm'
+copyright = u'2013, Alexander Dupuy'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -184,7 +183,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', '.tex', u' Documentation',
-   u'ChangeToMyName', 'manual'),
+   u'Alexander Dupuy', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,7 +213,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', '', u' Documentation',
-     [u'ChangeToMyName'], 1)
+     [u'Alexander Dupuy'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -228,7 +227,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', '', u' Documentation',
-   u'ChangeToMyName', '', 'One line description of project.',
+   u'Alexander Dupuy', '', 'One line description of project.',
    'Miscellaneous'),
 ]
 
